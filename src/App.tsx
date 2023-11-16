2d5e988f6a37103c0ba3f24f2b33a8122c0ae443
null <null>
2023-11-16 18:13:12 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { useState } from 'react';
 
 
-
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -12,13 +11,11 @@ const trackUrls = [
   'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
 ];
 
-
 const [trackIndex, setTrackIndex] = useState(0)
 
 const goToNextTrack = () => {
   setTrackIndex(trackIndex + 1);
 }
-
 
 const App = () => {
   return (
